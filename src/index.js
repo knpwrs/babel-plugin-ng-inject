52b8ee93bc395090ae1d23c208c196954a12cefc
Kenneth Powers <ken@kenpowers.net>
2016-05-03 12:21:33 -0400

    Add support for arrow functions.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import _ from 'lodash';
  */
 const isInjectDirective = d => d.value.value === 'ngInject';
 
+
 /**
  * Babel plugin to automatically annotate functions for Angular dependency injection.
  * @param {Object} babel The current babel object.
@@ -13,22 +14,24 @@ const isInjectDirective = d => d.value.value === 'ngInject';
  * @return {Object} Babel visitor.
  */
 export default function ({ types: t }) {
+  const functionVisitor = path => {
+    // Check if we should inject
+    if (!_.some(path.node.body.directives, isInjectDirective)) return;
+    // Remove ngInject directive
+    path.node.body.directives = _.reject( // eslint-disable-line no-param-reassign
+      path.node.body.directives,
+      isInjectDirective
+    );
+    // Construct array for injection annotations
+    const arr = _.map(path.node.params, p => t.stringLiteral(p.name));
+    arr.push(path.node);
+    // Replace function with array
+    path.replaceWith(t.arrayExpression(arr));
+  };
   return {
     visitor: {
-      FunctionExpression(path) {
-        // Check if we should inject
-        if (!_.some(path.node.body.directives, isInjectDirective)) return;
-        // Remove ngInject directive
-        path.node.body.directives = _.reject( // eslint-disable-line no-param-reassign
-          path.node.body.directives,
-          isInjectDirective
-        );
-        // Construct array for injection annotations
-        const arr = _.map(path.node.params, p => t.stringLiteral(p.name));
-        arr.push(path.node);
-        // Replace function with array
-        path.replaceWith(t.arrayExpression(arr));
-      },
+      ArrowFunctionExpression: functionVisitor,
+      FunctionExpression: functionVisitor,
     },
   };
 }
--- test/index.test.js
+++ test/index.test.js
@@ -24,6 +24,16 @@ test('it should inject when ngInject directive is present', t => {
   t.end();
 });
 
+test('it should work for arrow functions', t => {
+  const input = clean`angular.module('MyMod').controller('MyCtrl', ($scope, $timeout) => {
+    'ngInject';
+  })`;
+  const output = run(input);
+  const expected = clean`angular.module('MyMod').controller('MyCtrl', ['$scope', '$timeout', ($scope, $timeout) => {}]);`;
+  t.equal(output, expected);
+  t.end();
+});
+
 test('it should leave everything alone when ngInject directive is not present', t => {
   const input = clean`angular.module('MyMod').controller('MyCtrl', function ($scope, $timeout) {
   });`;
