2b85e9c8b5170ac72d5ca64778ba6740d97cd582
Kenneth Powers <ken@kenpowers.net>
2016-05-03 12:05:09 -0400

    Basic functionality.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-export default true;
+import _ from 'lodash';
+
+/**
+ * Determines if the given directive is an 'ngInject' directive.
+ * @param {Object} d The Directive node from Babylon.
+ */
+const isInjectDirective = d => d.value.value === 'ngInject';
+
+/**
+ * Babel plugin to automatically annotate functions for Angular dependency injection.
+ * @param {Object} babel The current babel object.
+ * @param {Object} babel.types babel-types
+ * @return {Object} Babel visitor.
+ */
+export default function ({ types: t }) {
+  return {
+    visitor: {
+      FunctionExpression(path) {
+        // Check if we should inject
+        if (!_.some(path.node.body.directives, isInjectDirective)) return;
+        // Remove ngInject directive
+        path.node.body.directives = _.reject( // eslint-disable-line no-param-reassign
+          path.node.body.directives,
+          isInjectDirective
+        );
+        // Construct array for injection annotations
+        const arr = _.map(path.node.params, p => t.stringLiteral(p.name));
+        arr.push(path.node);
+        // Replace function with array
+        path.replaceWith(t.arrayExpression(arr));
+      },
+    },
+  };
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,33 @@
+/* eslint max-len: "off" */
 import { test } from 'tape';
+import { transform } from 'babel-core';
 import plugin from '../src';
 
-test('it should work', t => {
-  t.equal(plugin, true);
+function clean([code]) {
+  return code.replace(/\n\s*/g, '');
+}
+
+function run(code) {
+  return clean([transform(code, {
+    plugins: [plugin],
+  }).code]);
+}
+
+test('it should inject when ngInject directive is present', t => {
+  const input = clean`angular.module('MyMod').controller('MyCtrl', function ($scope, $timeout) {
+    'ngInject';
+  });`;
+  const output = run(input);
+  const expected = clean`angular.module('MyMod').controller('MyCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+  }]);`;
+  t.equal(output, expected);
+  t.end();
+});
+
+test('it should leave everything alone when ngInject directive is not present', t => {
+  const input = clean`angular.module('MyMod').controller('MyCtrl', function ($scope, $timeout) {
+  });`;
+  const output = run(input);
+  t.equal(output, input);
   t.end();
 });
